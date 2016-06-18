dbda7df79f133fe97a97a69b39c71a1c936a024f
Austin Dunn <awd123ps@gmail.com>
2016-06-18 13:24:14 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python3
 import requests
+from urllib.parse import quote_plus
+
+r = requests.get(quote_plus(http://www.collegehumor.com/video/7017501/last-jake-and-amir-episode-ever))
