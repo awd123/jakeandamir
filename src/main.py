5e0fa66e29c82234f2906817ad66c62d48f8fe90
Austin Dunn <awd123ps@gmail.com>
2016-06-18 13:26:26 -0400

    Fix quoting

--- src/main.py
+++ src/main.py
@@ -2,4 +2,4 @@
 import requests
 from urllib.parse import quote_plus
 
-r = requests.get(quote_plus(http://www.collegehumor.com/video/7017501/last-jake-and-amir-episode-ever))
+r = requests.get(quote_plus("http://www.collegehumor.com/video/7017501/last-jake-and-amir-episode-ever"))
