cc7e3c6264daf126958bfd8a3c903fa60d1fddd7
Austin Dunn <awd123ps@gmail.com>
2016-06-19 23:29:27 -0400

    Add JSON

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-import ch_api as ch
+import json # JSON functions
+import ch_api as ch # CollegeHumor API functions
 
 video = ch.Video("7017501/last-jake-and-amir-episode-ever")
 
-video_response = video.get()
+video_dict = video.get().json() # returns a Dictionary of the JSON returned by
+                                # the CH API
+
+print(video_dict["title"])
